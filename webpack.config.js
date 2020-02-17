c18eeb953f920a836f7a5fed4601c44e03d347d7
Timur Ismagilov <ismagilov42@yandex.ru>
2020-02-17 13:28:20 +0300

    Убрал лишние отступы

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano'),
             cssProcessorPluginOptions: {
-                    preset: ['default'],
+                preset: ['default'],
             },
             canPrint: true
         }),
